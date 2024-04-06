4bde7250754c6a3ee50a9bd79e18bffc93c626e5
Randy Eash <reashlv@icloud.com>
2024-04-06 14:04:40 -0700

    Range and bearing

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem "sprockets-rails"
 #gem "sqlite3", "~> 1.4"
 gem "pg"
 
+gem 'math'
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,13 @@
 class Ship < ApplicationRecord
+  def range(otherShipID)
+    otherShip = Ship.find(otherShipID)
+      Math.sqrt((otherShip.x_location - x_location)**2 + (otherShip.y_location - y_location)**2) 
+  end
+  def bearing (otherShipID)
+    otherShip = Ship.find(otherShipID)
+    distance = range(otherShipID)
+    sign = (otherShip.y_location - y_location) / distance
+    radian = Math.asin(sign)
+    radian * 180 / Math::PI
+  end
 end
